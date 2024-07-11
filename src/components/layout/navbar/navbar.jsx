fe9b0038253117c8377a9450cebea0373380fdfb
Najibmansouur <najibmansour2005@gmail.com>
2024-07-12 01:21:48 +0300

    ADD: nav hover

--- src/components/layout/navbar/navbar.jsx
+++ src/components/layout/navbar/navbar.jsx
@@ -16,12 +16,11 @@ const Navbar = () => {
     <div className="w-full absolute p-6 z-50">
       <nav className="lg:flex hidden text-4xl w-full justify-around">
         {routes.map(({ title, href }) => (
-          <Link
-            key={title}
-            href={href}
-            className="hover:text-secondary hover:underline underline-offset-8 transition-all"
-          >
-            {title}
+          <Link key={title} href={href} className="relative group ">
+            <span className="cursor-pointer transition-all duration-300 hover:text-secondary">
+              {title}
+            </span>
+            <div className="absolute bottom-0 left-1/2 w-0 h-0.5 bg-secondary  duration-300 ease-out group-hover:w-full -translate-x-1/2" />
           </Link>
         ))}
       </nav>
